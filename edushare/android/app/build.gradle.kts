cadf7434d6732467143b4d37ef4b1bf18de955cd
Vibhas Bhaskar <vibhas.bhaskar@gmail.com>
2025-09-29 22:08:28 -0700

    Got firebase working on Android

--- edushare/android/app/build.gradle.kts
+++ edushare/android/app/build.gradle.kts
@@ -23,10 +23,8 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        // TODO: Specify our own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.edushare"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -35,8 +33,7 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+            // TODO: Add our own signing config for the release build.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -44,10 +41,4 @@ android {
 
 flutter {
     source = "../.."
-}
-
-apply plugin: 'com.google.gms.google-services'
-
-android {
-
 }
